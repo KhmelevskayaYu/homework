67ee4d934216cb5accccac4049631887cba57bc8
KhmelevskayaYu <hmyu011@gmail.com>
2019-03-24 10:27:13 -0400

    changed codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream> //some includes
 #include <string>
 
-int main(){ //function main
-	std::string name; // string variable
-	getline(std::cin, name); // username promt
-	std::cout<<"Hello world from "<<name<<std::endl;// printing message
-	return 0;
+int
+main()
+{                                                        // function main
+  std::string name;                                      // string variable
+  getline(std::cin, name);                               // username promt
+  std::cout << "Hello world from " << name << std::endl; // printing message
+  return 0;
 }
